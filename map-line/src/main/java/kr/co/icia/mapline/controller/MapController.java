ae80915264f654ce2255a398b8da273045c113e0
seokjaegon <seokjaegon@naver.com>
2024-01-23 16:30:46 +0900

    path 주석

--- map-line/src/main/java/kr/co/icia/mapline/controller/MapController.java
+++ map-line/src/main/java/kr/co/icia/mapline/controller/MapController.java
@@ -26,23 +26,34 @@ public class MapController {
 	 * 
 	 */
 	@GetMapping("/map/paths") // url : /map/paths
-	public String getMapPaths(@RequestParam(required = false) String fromAddress, //
-			@RequestParam(required = false) String toAddress, //
-			Model model) throws IOException, InterruptedException {
+	public String getMapPaths(@RequestParam(required = false) String fromAddress, // 출발지 주소 정보를 요청 파라미터로 받아온다
+			@RequestParam(required = false) String toAddress, // 도착지 주소 정보를 요청 파라미터로 받아온다.
+			Model model // 뷰에 데이터를 전달하기 위한 객체이다.
+			) throws IOException, InterruptedException {
+		// fromPoint(출발지), toPoint(도착지) 초기값 설정
 		Point fromPoint = null;
 		Point toPoint = null;
+		// 만약 출발지 주소가 null이 아니고 비어있지 않으면
 		if (fromAddress != null && !fromAddress.isEmpty()) {
+			// fromPoint(출발지)에 출발지 주소의 위도와 경도 값을 받는다.
 			fromPoint = KakaoApiUtil.getPointByAddress(fromAddress);
+			// "fromPoint"라는 변수로 fromPoint(출발지)의 값을 paths.html로 전달한다.
 			model.addAttribute("fromPoint", fromPoint);
 		}
+		// 만약 도착지 주소가 null이 아니고 비어있지 않으면
 		if (toAddress != null && !toAddress.isEmpty()) {
+			// toPoint(도착지)에 도착지 주소의 위도와 경도 값을 받는다.
 			toPoint = KakaoApiUtil.getPointByAddress(toAddress);
+			// "toPoint"라는 변수로 toPoint(도착지)의 값을 paths.html로 전달한다.
 			model.addAttribute("toPoint", toPoint);
 		}
-
+		// 만약 fromPoint(출발지)와 toPoint(도착지) 둘다 null이 아니면
 		if (fromPoint != null && toPoint != null) {
+			// pointList에 fromPoint(출발지)에서 toPoint(도착지)까지의 차량경로를 저장한다.
 			List<Point> pointList = KakaoApiUtil.getVehiclePaths(fromPoint, toPoint);
+			// pointList를 JSON 형식의 문자열로 반환한다.
 			String pointListJson = new ObjectMapper().writer().writeValueAsString(pointList);
+			// "pointList"라는 이름으로 pointListJosn을 paths.html로 전달한다.
 			model.addAttribute("pointList", pointListJson);
 		}
 		return "map/paths";
